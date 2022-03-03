81fb4aef33abdc9035fa29388db3e7dc2cae1648
Yogesh Bajgude <55928894+ybpatil@users.noreply.github.com>
2022-03-03 12:07:59 +0530

    Add files via upload

---
+++ Core Java/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("How are you");
+
+        int a = 22;
+        int b = 32;
+
+        int sum = a + b;
+
+        System.out.println("Sum is : "+ sum);
+    }
+}
